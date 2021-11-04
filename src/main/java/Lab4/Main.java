2e3fd13e28a37bc20ef783d412082f5accfe5a62
Андрей <evstratevandrey@gmail.com>
2021-11-04 21:19:48 +0200

    add main

---
+++ src/main/java/Lab4/Main.java
@@ -0,0 +1,26 @@
+package Lab4;
+
+import java.util.Random;
+
+public class Main {
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        Thread downloadThread = new DownloadThread();
+        downloadThread.start();
+        try {
+            downloadThread.join();
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+        APIData data = DownloadThread.data;
+        int i = random.nextInt(data.getData().size());
+        String Anime_name = data.getData().get(i).getAnimeName();
+        APIGetter apiGetter = new APIGetter();
+        APIGetter.url = "https://anime-facts-rest-api.herokuapp.com/api/v1/" + Anime_name;
+        apiGetter.run();
+        Facts facts = (Facts) apiGetter.JsonParse(Facts.class);
+        System.out.println("Anime: " + Anime_name);
+        System.out.println(facts);
+    }
+}
