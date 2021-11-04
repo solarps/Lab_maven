f19a1b546a8d420a109deeed4cc8d778b2a40ff7
Андрей <evstratevandrey@gmail.com>
2021-11-04 18:06:36 +0200

    fix's

--- src/main/java/Lab4/APIGetter.java
+++ src/main/java/Lab4/APIGetter.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class APIGetter implements Runnable{
     public String jsonIn;
@@ -20,13 +20,13 @@ public class APIGetter implements Runnable{
         return sb.toString();
     }
 
-    public String ConnectAndGetData() {
+    public synchronized String ConnectAndGetData() {
         jsonIn = "";
-        InputStream is = null;
+        InputStream is;
         try {
             is = new URL(url).openStream();
             try {
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
                 try {
                     jsonIn = readAll(rd);
                 } catch (IOException e) {
@@ -45,16 +45,11 @@ public class APIGetter implements Runnable{
         return jsonIn;
     }
 
-    public Object JsonParse(int type) {
+    public synchronized Object JsonParse(Class<?> type) {
         ObjectMapper mapper = new ObjectMapper();
         Object data = null;
         try {
-            if (type == 1) {
-                data = mapper.readValue(jsonIn, APIData.class);
-            }
-            if (type == 2) {
-                data = mapper.readValue(jsonIn, Facts.class);
-            }
+            data = mapper.readValue(jsonIn, type);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
--- src/main/java/Lab4/DownloadThread.java
+++ src/main/java/Lab4/DownloadThread.java
@@ -1,10 +1,12 @@
 package Lab4;
 
 public class DownloadThread extends Thread{
-    static APIGetter apiGetter = new APIGetter();
+    static APIData data;
     @Override
     public void run(){
+        APIGetter apiGetter = new APIGetter();
         APIGetter.url = "https://anime-facts-rest-api.herokuapp.com/api/v1";
         apiGetter.ConnectAndGetData();
+        data = (APIData) apiGetter.JsonParse(APIData.class);
     }
 }
